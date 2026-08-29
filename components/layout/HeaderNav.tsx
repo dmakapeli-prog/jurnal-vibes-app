87abd6a0c20d4d2df7f68000ca6a8254c4302501
Donie Makapeli <donie.makapeli_ti@nusaputra.ac.id>
2026-08-29 16:44:33 +0700

    style: Explicit logo wrapper flex items-center and h-12 w-auto md:h-14 mix-blend-multiply

--- components/layout/HeaderNav.tsx
+++ components/layout/HeaderNav.tsx
@@ -39,14 +39,16 @@ export const HeaderNav: React.FC = () => {
       <div className="flex justify-between items-center px-margin-mobile md:px-margin-desktop h-20 max-w-container-max mx-auto w-full relative z-10">
         {/* Brand Logo */}
         <div className="flex-shrink-0 w-1/4 flex items-center">
-          <Link href="/" className="flex items-center">
-            {/* eslint-disable-next-img-element */}
-            <img
-              alt="Jurnal Vibes Logo"
-              className="h-10 md:h-14 w-auto object-contain mix-blend-multiply"
-              src="https://lh3.googleusercontent.com/aida-public/AB6AXuA_G4cy03YWhYa-Kym40jgbZSqmdZP7gNs-gp6i_alZS2lNYY8I-Yia7oaBcFZoHsd3u4ZjnFrGMnyRVa-lQTAFhRgUmxfrCMyotmWtSVTRq4GNhvL-4FdQWSBvinHo70fsaO1VQmUfKSKvHZU5L8dK0y4yNLh0VMRtPqobABdBRTvJRkuwqUKma6mTv0LZBFEKz-jheCGRyqIvjdGAyouWnTZK3lnRRPmS__HVL3w6UyhZN3-lKwGDF8WTdsHZi3iNPZ0"
-            />
-          </Link>
+          <div className="flex items-center">
+            <Link href="/" className="flex items-center">
+              {/* eslint-disable-next-img-element */}
+              <img
+                alt="Jurnal Vibes Logo"
+                className="h-12 w-auto md:h-14 object-contain mix-blend-multiply"
+                src="https://lh3.googleusercontent.com/aida-public/AB6AXuA_G4cy03YWhYa-Kym40jgbZSqmdZP7gNs-gp6i_alZS2lNYY8I-Yia7oaBcFZoHsd3u4ZjnFrGMnyRVa-lQTAFhRgUmxfrCMyotmWtSVTRq4GNhvL-4FdQWSBvinHo70fsaO1VQmUfKSKvHZU5L8dK0y4yNLh0VMRtPqobABdBRTvJRkuwqUKma6mTv0LZBFEKz-jheCGRyqIvjdGAyouWnTZK3lnRRPmS__HVL3w6UyhZN3-lKwGDF8WTdsHZi3iNPZ0"
+              />
+            </Link>
+          </div>
         </div>
 
         {/* Navigation Links */}
